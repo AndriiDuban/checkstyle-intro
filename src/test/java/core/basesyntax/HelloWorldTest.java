609663bbf1db8450dfb8b9f2ed92785a7b67a385
AndriiDuban <andr.duban@gmail.com>
2025-10-06 20:47:00 +0000

    Master Я написав тест класу

--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,14 @@
 package core.basesyntax;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 /**
  * Feel free to remove this class and create your own.
  */
 public class HelloWorldTest {
 
+ @Test
+ public void TestHelloWorld() {
+    assertEquals("Hello World", someMethod());
+ }
 }
